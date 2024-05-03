2e5c608d404d1894325668e5eb7a0280ea3b7366
thuannguyen113123 <91785118+thuannguyen113123@users.noreply.github.com>
2024-05-03 20:03:33 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ app.use("/api/user", userRoutes);
 // bài viết
 app.use("/api/post", postRoutes);
 
-app.use(express.static(path.join(__dirname, "./client/dist")));
+app.use(express.static(path.join(__dirname, "../client/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
